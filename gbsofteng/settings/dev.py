9a454c8e2442bb072d47f11eb01dfb5fb239ebd6
German Bobadilla <germanbobadilla@gmail.com>
2025-08-16 21:31:20 -0400

    creating home

---
+++ gbsofteng/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-o0!rf(&!)dluf@_vh_)$k6vbz!v1f!)xh8ihj40s)nvj+p=#6_"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
